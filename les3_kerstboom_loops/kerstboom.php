f1fd75e49e86b4afce92fe3b4fc6a4dec0e58fdf
LisaGroen <lgroen@roc-dev.com>
2017-02-16 09:02:57 +0100

    ^^

--- les3_kerstboom_loops/kerstboom.php
+++ les3_kerstboom_loops/kerstboom.php
@@ -1,31 +1,11 @@
+<center>
 <?php
 
 $regel = 9;
 
 for ($i = 0; $i < $regel; $i++) {
-    echo "*<br>";
-    for ($i = 0; $i < $regel - 1; $i++) {
-        echo "**<br>";
-        for ($i = 0; $i < $regel - 2; $i++) {
-            echo "***<br>";
-            for ($i = 0; $i < $regel - 3; $i++) {
-                echo "****<br>";
-                for ($i = 0; $i < $regel - 4; $i++) {
-                    echo "*****<br>";
-                    for ($i = 0; $i < $regel - 5; $i++) {
-                        echo "******<br>";
-                        for ($i = 0; $i < $regel - 6; $i++) {
-                            echo "*******<br>";
-                            for ($i = 0; $i < $regel - 7; $i++) {
-                                echo "********<br>";
-                                for ($i = 0; $i < $regel - 8; $i++) {
-                                    echo "*********<br>";
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
+    for ($j = 0; $j < $i; $j++) {
+        echo "*";
     }
+    echo "<br>";
 }
