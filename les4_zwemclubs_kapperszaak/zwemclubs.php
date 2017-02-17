84a583d4e28953d8ff109d54b8ac54ff8b70fcd5
LisaGroen <lgroen@roc-dev.com>
2017-02-17 10:34:48 +0100

    ^^

--- les4_zwemclubs_kapperszaak/zwemclubs.php
+++ les4_zwemclubs_kapperszaak/zwemclubs.php
@@ -1,2 +1,38 @@
 <?php
 
+$spartelkuikens = 25;
+$waterbuffels = 32;
+$plonsmderin = 11;
+$bommetje = 23;
+
+echo "De spartelkuikens";
+for ($i = 1; $i <= $spartelkuikens; $i++){
+    if ($i % 5 ==0) {
+        echo "<img src='zwemmer.png'>";
+    }
+}
+echo "<br>";
+
+echo "De waterbuffels";
+for ($i = 1; $i <= $waterbuffels; $i++){
+    if ($i % 5 ==0) {
+        echo "<img src='zwemmer.png'>";
+    }
+}
+echo "<br>";
+
+echo "Plonsmderin";
+for ($i = 1; $i <= $plonsmderin; $i++){
+    if ($i % 5 ==0) {
+        echo "<img src='zwemmer.png'>";
+    }
+}
+echo "<br>";
+
+echo "Bommetje";
+for ($i = 1; $i <= $bommetje; $i++){
+    if ($i % 5 ==0) {
+        echo "<img src='zwemmer.png'>";
+    }
+}
+echo "<br>";
