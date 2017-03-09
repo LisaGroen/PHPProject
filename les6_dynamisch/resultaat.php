d71637c50518dedb3cbaf0a96bd6cbecfd596aa0
LisaGroen <lgroen@roc-dev.com>
2017-03-09 08:53:14 +0100

    ^^

--- les6_dynamisch/resultaat.php
+++ les6_dynamisch/resultaat.php
@@ -1,6 +1,16 @@
 <?php
-echo "Naam: ".$_GET["naam"]."<br>";
-echo "Adres: ".$_GET["adres"]."<br>";
-echo "Email: ".$_GET["email"]."<br>";
 
+include("form.html");
+
+if (strlen($_GET["naam"]) == 0 || strlen($_GET["adres"]) == 0 || strlen($_GET["email"]) == 0 || strlen($_GET["wachtwoord"]) == 0) {
+
+    echo "<br>";
+    echo "Je moet eerst het formelier invullen";
+}
+else {
+    echo "<br>";
+    echo "Naam: " . $_GET["naam"] . "<br>";
+    echo "Adres: " . $_GET["adres"] . "<br>";
+    echo "Email: " . $_GET["email"] . "<br>";
+}
 
